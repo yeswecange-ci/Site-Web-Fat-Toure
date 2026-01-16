aff1dfaddceece5300dec829bfecd3be8686fc38
Josii13 <ncho.astejosias@gmail.com>
2026-01-16 21:44:36 +0100

    Require PHP 8.3

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,8 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-// Frontend routes avec locale
-Route::middleware([\App\Http\Middleware\SetLocale::class])->group(function () {
+// Frontend routes
+Route::middleware(['web', \App\Http\Middleware\SetLocale::class])->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('home');
     Route::get('/locale/{locale}', [HomeController::class, 'switchLocale'])->name('locale.switch');
 });
-
-// Les routes Filament (/admin) sont enregistrées automatiquement par le provider
